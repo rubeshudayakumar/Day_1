86cfc4bca434bbb86ea1536ad919074546d48c88
rubeshudayakumar <rubesh.udayakumar@cdw.com>
2024-11-27 21:54:58 +0530

    Initial commit

---
+++ src/ArmstrongNumber.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int number = scanner.nextInt();
+        System.out.println(number);
+    }
+}
