16e67244a1f66d1f2e1b5edc21c04dcb442e582e
rubeshudayakumar <rubesh.udayakumar@cdw.com>
2024-11-27 22:09:18 +0530

    completed 4th program

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,45 @@
 import java.util.Scanner;
 
 public class Calculator {
+
+    private final Operation operation;
+    private final int firstNumber;
+    private final int secondNumber;
+
+    private Calculator(String operation, int firstNumber, int secondNumber){
+        this.operation = Operation.valueOf(operation.toUpperCase());
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+    }
+
+    private static enum Operation{
+        ADD,
+        SUBTRACT,
+        MULTIPLY,
+        DIVIDE,
+        MODULO
+    }
+
+    private void calculate(){
+        switch (this.operation) {
+            case ADD ->{
+                System.out.println(this.firstNumber + this.secondNumber);
+            }
+            case SUBTRACT ->{
+                System.out.println(this.firstNumber - this.secondNumber);
+            }
+            case MULTIPLY ->{
+                System.out.println(this.firstNumber * this.secondNumber);
+            }
+            case DIVIDE -> {
+                System.out.println(this.firstNumber / this.secondNumber);
+            }
+            case MODULO -> {
+                System.out.println(this.firstNumber % this.secondNumber);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String operation;
@@ -9,8 +48,8 @@ public class Calculator {
         firstNumber = scanner.nextInt();
         secondNumber = scanner.nextInt();
 
-
-
+        Calculator calculator = new Calculator(operation,firstNumber,secondNumber);
+        calculator.calculate();
 
     }
 }
